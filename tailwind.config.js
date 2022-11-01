9202cd058a37b4d41fee128647c123637d9aaf8a
Camille C <camara.camille@gmail.com>
2022-11-01 12:29:45 +0100

    1st page commmit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ogg: ["ogg", "sans-serif"],
+        apercu: ["apercu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
