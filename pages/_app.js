f5f3589c7578bca652ab352389cf78fc0ae8eb9c
Camille C <camara.camille@gmail.com>
2022-11-16 14:05:59 +0100

    Test reacttransitiongroup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import PageTransitions from "../components/PageTransitions";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <PageTransitions route={router.asPath}>
+      <Component {...pageProps} />
+    </PageTransitions>
+  );
 }
 
-export default MyApp
+export default MyApp;
